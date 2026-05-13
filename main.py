239fe0119b7dfbd7c81b1d0c3952b4ab0bf6cfb0
jasnoorranu619-dev <jasnoorr965@deltalearns.ca>
2026-05-13 09:49:31 -0700

    added functions
    going to add title labels

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ window = tk.Tk()
 window.title("Calculator App")
 window.geometry("400x400")
 window.configure(bg="lightgray") 
+
+# ---------------- FUNCTIONS ----------------
+def calculate():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+
+        operation = math_choice.get()
+
+        if operation == "+":
+            answer = num1 + num2
+        elif operation == "-":
+            answer = num1 - num2
+        else:
+            answer = "Choose an operation"
+
+        answer_label.config(text=f"Answer: {answer}")
+
+    except:
+        messagebox.showerror("Error", "Please enter valid numbers")
+
+
+def clear():
+    entry1.delete(0, tk.END)
+    entry2.delete(0, tk.END)
+    answer_label.config(text="Answer: ")
+    math_choice.set("")
