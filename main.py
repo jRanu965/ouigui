b27d209c3bc23e056a67bb9b13e09342350387a7
jasnoorranu619-dev <jasnoorr965@deltalearns.ca>
2026-05-19 10:32:24 -0700

    added all the psudocode

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Addition and Subtraction Calculator
-# Save as calculator.py
-
 import tkinter as tk
 from tkinter import messagebox
 
@@ -8,34 +5,50 @@ from tkinter import messagebox
 window = tk.Tk()
 window.title("Calculator App")
 window.geometry("400x400")
-window.configure(bg="lightgray") 
+window.configure(bg="lightgray")
 
 # ---------------- FUNCTIONS ----------------
 def calculate():
+
+    # Try to convert user input into numbers
     try:
         num1 = float(entry1.get())
         num2 = float(entry2.get())
 
+        # Get selected operation from radio buttons
         operation = math_choice.get()
 
+        # Check which operation user selected
         if operation == "+":
             answer = num1 + num2
+
         elif operation == "-":
             answer = num1 - num2
+
         else:
             answer = "Choose an operation"
 
+        # Display answer on screen
         answer_label.config(text=f"Answer: {answer}")
 
+    # If user enters invalid data
     except:
         messagebox.showerror("Error", "Please enter valid numbers")
 
 
+# Function to clear all fields
 def clear():
+
+    # Remove text from entry boxes
     entry1.delete(0, tk.END)
     entry2.delete(0, tk.END)
+
+    # Reset answer label
     answer_label.config(text="Answer: ")
+
+    # Remove selected radio button
     math_choice.set("")
+
 
 # ---------------- TITLE LABEL ----------------
 title_label = tk.Label(
@@ -49,7 +62,6 @@ title_label.pack(pady=10)
 # ---------------- FRAME ----------------
 input_frame = tk.Frame(window, bg="lightgray")
 input_frame.pack(pady=15)
-
 
 # ---------------- FIRST NUMBER ----------------
 label1 = tk.Label(
@@ -96,8 +108,10 @@ operation_label = tk.Label(
 )
 operation_label.pack(pady=5)
 
+# Variable to store selected radio button
 math_choice = tk.StringVar()
 
+# Addition radio button
 radio_add = tk.Radiobutton(
     window,
     text=" Addition (+)",
@@ -109,6 +123,7 @@ radio_add = tk.Radiobutton(
 )
 radio_add.pack()
 
+# Subtraction radio button
 radio_subtract = tk.Radiobutton(
     window,
     text=" Subtraction (-)",
